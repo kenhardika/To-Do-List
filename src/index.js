8217dc12e4a597f4bcdf29d208212032bb2d71d6
kenhardika <kenzamahardika@gmail.com>
2022-06-17 19:28:30 +0700

    add initial script

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './style.css';
 console.log('here we are');
+
+
+function addToDoList(title, desc, dueDate, priority){
+
+    const showTitle = () => {console.log(title)};
+
+
+    return {
+        showTitle,
+        desc,
+        dueDate,
+        priority
+    }
+
+}
+
+const test = addToDoList('The Title Is a Title', 'This is a title description', '12/12/12', 'PRIORITY NUMBER ONE');
+
+test.showTitle();
+console.log(test.desc);
