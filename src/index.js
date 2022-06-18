e007f52d019ac56fe225895d9ab4407c542fd6bd
kenhardika <kenzamahardika@gmail.com>
2022-06-18 08:40:22 +0700

    add func testing

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ console.log('here we are');
 
 function addToDoList(title, desc, dueDate, priority){
 
-    const showTitle = () => {console.log(title)};
-
+    const notPrint = () => { 
+        console.log('should not be printed if this not called');
+     }
 
     return {
-        showTitle,
+        title,
         desc,
         dueDate,
-        priority
+        priority,
+        showTitle(){
+            return `THIS IS THE TITLE ${title}`;
+            }
     }
-
 }
 
 const test = addToDoList('The Title Is a Title', 'This is a title description', '12/12/12', 'PRIORITY NUMBER ONE');
-
-test.showTitle();
-console.log(test.desc);
+console.log(test.showTitle());
