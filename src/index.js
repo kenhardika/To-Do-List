39e07b66893bb12ab966457dcb18b0e4d99e2881
kenhardika <kenzamahardika@gmail.com>
2022-06-19 17:00:53 +0700

    update script add To Do List basic logic

--- src/index.js
+++ src/index.js
@@ -1,60 +1,89 @@
 import './style.css';
 console.log('here we are');
 
-
-function addToDoList(title, desc, dueDate, priority){
-
-    const notPrint = () => { 
-        console.log('should not be printed if this not called');
-     }
-
+//gather data input tempelate
+function addToDoList(title, desc = '', dueDate, priority = 'Low', checklist = false){ 
     return {
         title,
         desc,
         dueDate,
         priority,
-        showTitle(){
-            return `THIS IS THE TITLE ${title}`;
-            }
+        checklist
     }
 }
 
-const test = addToDoList('The Title Is a Title', 'This is a title description', '12/12/12', 'PRIORITY NUMBER ONE');
-console.log(test.showTitle());
-console.log('TESTING');
-
-
-class ArrayToDoList {
-    constructor(){
-        this.array = [];
-    }
-
-    pushToArray(elem){
-        this.array.push(elem);
-        console.log(this.array);
-    }
-
-    callTheArray() {
-        return this.array;
-    }
-}
-
-const addToArray = new ArrayToDoList();
-addToArray.pushToArray(test);
-const kids = addToDoList('Bangun Pagi', 'Wake up in the morning for school', 'everyday', 'High');
-addToArray.pushToArray(kids);
-
-
-function arrayToDo() {
+//create Array to save to do list tempelate
+function arrayToDoList() {
     let arrayList = [];
+
+    function showArrayList(){
+        console.log(arrayList);
+    }
+
+    function addToArrayList(data){
+        arrayList.push(data) 
+    }
+
+    function removeFromArrayList(removedTitle){
+       return arrayList = arrayList.filter((array)=> array.title !== removedTitle);
+    }
+
     return{
-        addToArrayList: (element)=> { arrayList.push(element) },
-        showArrayList: console.log(arrayList),
-        arrayList
+        arrayList,
+        showArrayList,
+        addToArrayList,
+        removeFromArrayList
     }
 }
 
-const myArray = arrayToDo();
-myArray.addToArrayList('data1');
-myArray.addToArrayList('data2');
-console.log(myArray.arrayList)
+//The Logic after you click add 
+
+const myArray = arrayToDoList();
+
+function actionNewToDoList(){
+
+    //create Array for To Do List
+    //create To Do List
+    let task1 = addToDoList('Jemput', 'Jemput keluarga besar di bandara', '09:00 - 12/12/12');
+    //add the variable of to do list to the aray 
+    myArray.addToArrayList(task1);
+    task1 = addToDoList('Pulang', 'Anter aja pak', '19:00 - 12/12/12');
+   
+    myArray.addToArrayList(task1);
+    myArray.showArrayList(); 
+}
+
+actionNewToDoList();
+
+
+
+
+
+
+
+
+
+// const task1 = addToDoList('Jemput', 'Jemput keluarga besar di bandara', '09:00 - 12/12/12');
+// console.log(task1)
+// const myObj = { title: 'Jemput', desc: 'Jemput keluarga besar di bandara', dueDate: '09:00 - 12/12/12', priority: 'Urgent'};
+// console.log(myObj);
+
+////// IF U USE CLASS
+// class ArrayToDoList {
+//     constructor(){
+//         this.array = [];
+//     }
+
+//     pushToArray(elem){
+//         this.array.push(elem);
+//         console.log(this.array);
+//     }
+
+//     callTheArray() {
+//         return this.array;
+//     }
+// }
+
+// const addToArray = new ArrayToDoList();
+// addToArray.pushToArray(test);
+// addToArray.pushToArray(kids);
